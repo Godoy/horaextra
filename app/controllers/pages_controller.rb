ece8f3010105bf4982190d406cf2c50b5cd85ff4
Adriano Godoy <godoy.ccp@gmail.com>
2013-02-24 17:59:00 -0300

    Negando cadastros que nao possuam o dominio da Plan B Apps

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class PagesController < ApplicationController
   def home
   end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,24 @@
+# -*- coding: utf-8 -*-
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	def google_oauth2
 	    # You need to implement the method below in your model (e.g. app/models/user.rb)
 
 	    auth = request.env["omniauth.auth"]
-	    @user = User.find_for_google_oauth2(auth, current_user)
-	    
-	    #session["auth"] = auth
 
-	    if @user.persisted?
-	      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-	      sign_in_and_redirect @user, :event => :authentication
-	    else
-	      session["devise.google_data"] = auth 
-	      redirect_to new_user_registration_url
+	    if auth["extra"]['raw_info']['hd'] == "planb.com.br"
+		    @user = User.find_for_google_oauth2(auth, current_user)
+		    
+		    session["auth"] = auth
+
+		    if @user.persisted?
+		      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+		      sign_in_and_redirect @user, :event => :authentication
+		    else
+		      session["devise.google_data"] = auth 
+		      redirect_to new_user_registration_url
+		    end
+		else
+		   redirect_to root_url, notice: "Não permitido."
 	    end
 	end
 end
