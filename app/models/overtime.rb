d6318de4bf5c7e64e58721221f050cd0548919a6
ruby -v <luiz.alves@planb.com.br>
2013-03-28 10:23:11 -0300

    testando novo repositorio

--- app/models/overtime.rb
+++ app/models/overtime.rb
@@ -1,5 +1,5 @@
 class Overtime < ActiveRecord::Base
-  attr_accessible :description, :end_date, :end_time, :start_date, :start_time, :user_id, :project_id
+  attr_accessible :description, :end_date, :end_time, :start_date, :start_time, :user_id, :project_id, :default
 
   belongs_to :user, :inverse_of => :overtimes
   belongs_to :project
@@ -8,6 +8,8 @@ class Overtime < ActiveRecord::Base
   validates :description, :end_date, :end_time, :start_date, :start_time, :user_id, :project_id, :presence => true
 
   def calculator
+  	
   	Time.at(self.end_time - self.start_time).gmtime.strftime('%T')
+
   end
 end
