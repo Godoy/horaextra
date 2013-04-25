0497e44b7f6e6c50319cf8b32793e80c83e680bc
ruby -v <luiz.alves@planb.com.br>
2013-04-25 13:47:16 -0300

    Email

--- app/mailers/confirmation.rb
+++ app/mailers/confirmation.rb
@@ -1,10 +1,11 @@
 class Confirmation < ActionMailer::Base
-  default :from =>"luiz.alves@plan.com.br"
-  
+  default :from =>"luiz.alves@planb.com.br"
+  default :to => "lhlalves4@gmail.com"   
 
   def registration_confirm(user)
   	@user = user
-  	mail :to => "diego.griep@planb.com.br"
+  	mail(:subject => "[Cadastramento de Hora Extra] #{user.start_overtime}, #{user.end_overtime}
+  	#{user.description}")
   	
   end
 end
