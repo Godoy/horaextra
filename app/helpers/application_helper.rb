42cd12b79247ba69025b5d9600024812ebcccfc1
ruby -v <luiz.alves@planb.com.br>
2013-06-03 16:03:47 -0300

    calculo da hora

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def humanize secs
+    [[60, :segundos], [60, :minutos], [24, :horas], [1000, :dias]].map{ |count, name|
+    if secs > 0
+    secs, n = secs.divmod(count)
+    "#{n.to_i} #{name}"
+    end
+    }.compact.reverse.join(' ')
+  end
 end
